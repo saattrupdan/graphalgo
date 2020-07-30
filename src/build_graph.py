2f607a63589430140f7abd1b8ef6ecd68c2526af
Dan Saattrup Nielsen <saattrupdan@gmail.com>
2020-07-30 22:28:51 +0200

    feat: Build house graph

--- src/build_graph.py
+++ src/build_graph.py
@@ -1,8 +1,25 @@
 import networkx as nx
+import matplotlib.pyplot as plt
 
 def build_house() -> nx.Graph:
-    nodes = [1, 2, 3, 4, 5]
-    edges = [(1, 2), (2, 3), (3, 4), (4, 1), (2, 5), (5, 3)]
+
+    nodes = [
+        'bottom_left', 
+        'bottom_right', 
+        'top_left', 
+        'top_right', 
+        'roof'
+    ]
+
+    edges = [
+        ('bottom_left', 'top_left'), 
+        ('top_left', 'top_right'),
+        ('top_right', 'bottom_right'), 
+        ('bottom_right', 'bottom_left'),
+        ('top_left', 'roof'), 
+        ('roof', 'top_right')
+    ]
+
     graph = nx.Graph()
     graph.add_nodes_from(nodes)
     graph.add_edges_from(edges)
@@ -10,3 +27,5 @@ def build_house() -> nx.Graph:
 
 if __name__ == '__main__':
     graph = build_house()
+    nx.draw(graph)
+    plt.show()
