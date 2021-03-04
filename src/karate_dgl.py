41106d9306b44a655e3ae2018af3e3e2bf83997e
Dan Saattrup Nielsen <dansat@erst.dk>
2021-03-04 12:45:33 +0000

    feat: Karate changes

--- src/karate_dgl.py
+++ src/karate_dgl.py
@@ -11,6 +11,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch
 
+from sklearn.manifold import TSNE
+
 import dgl
 from dgl.nn.pytorch import GraphConv, GINConv, GATConv
 
@@ -33,6 +35,27 @@ def build_karate_club_graph() -> dgl.DGLGraph:
         24, 2, 23, 26, 1, 8, 0, 24, 25, 28, 2, 8, 14, 15, 18, 20, 22, 23,
         29, 30, 31, 8, 9, 13, 14, 15, 18, 19, 20, 22, 23, 26, 27, 28, 29, 30,
         31, 32]
+    )
+
+    # Edges are directional in DGL; Make them bi-directional.
+    u = np.concatenate([src, dst])
+    v = np.concatenate([dst, src])
+
+    # Construct a DGLGraph
+    return dgl.graph((u, v))
+
+def build_graph() -> dgl.DGLGraph:
+    ''' Build the famous karate club graph. '''
+
+    # All edges are stored in two numpy arrays. One for source endpoints
+    # while the other for destination endpoints.
+    src = np.array(
+        [0, 0, 0, 0, 0, 0, 3, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 8, 8, 8, 10, 13,
+            14, 17]
+    )
+    dst = np.array(
+        [1, 2, 3, 4, 5, 6, 4, 7, 8, 9, 6, 10, 11, 12, 13, 14, 15, 16, 17, 18,
+            19, 11, 20, 15, 18]
     )
 
     # Edges are directional in DGL; Make them bi-directional.
@@ -66,28 +89,27 @@ class GCN(nn.Module):
 
     def forward(self, graph):
         embeddings = graph.ndata['embeddings']
-        h0 = self.conv1(graph, embeddings)
-        h = torch.relu(h0)
+        h = self.conv1(graph, embeddings)
+        h = torch.relu(h)
         h = self.conv2(graph, h)
-        return h, h0
+        return h
 
 def train_net(graph, net, epochs: int, lr: float, make_animation: bool=False):
     nnodes = graph.number_of_nodes()
     embed = nn.Embedding(nnodes, net.emb_dim)
     graph.ndata['embeddings'] = torch.ones_like(embed.weight)
 
-    labelled_nodes = torch.tensor([15, 16, 0, 2])
+    labelled_nodes = torch.tensor([0, 15, 16, 2])
     labels = torch.tensor([0, 0, 1, 1])
 
     params = it.chain(net.parameters(), embed.parameters())
     optim = torch.optim.AdamW(params=params, lr=lr)
 
     all_logits = np.empty((epochs, nnodes, net.num_classes))
-    all_embeds = np.empty((epochs, nnodes, net.hidden_dim))
     for epoch in range(epochs):
         optim.zero_grad()
 
-        logits, embeds = net(graph)
+        logits = net(graph)
         log_probs = F.log_softmax(logits, 1)
         loss = F.nll_loss(log_probs[labelled_nodes], labels)
 
@@ -95,11 +117,9 @@ def train_net(graph, net, epochs: int, lr: float, make_animation: bool=False):
         optim.step()
 
         all_logits[epoch] = logits.detach().numpy()
-        all_embeds[epoch] = embeds.detach().numpy()
        
-        if epoch % 100 == 0:
+        if epoch % 10 == 0:
             print(f'Epoch {epoch} | Loss {loss.item():.4f}')
-
 
     #=== Make animation ===
     if make_animation:
@@ -109,7 +129,6 @@ def train_net(graph, net, epochs: int, lr: float, make_animation: bool=False):
 
         def draw(i: int):
             logits = all_logits[i]
-            embeds = all_embeds[i]
             classes = logits.argmax(-1)
             ncolours = [colours[cls] for cls in classes]
 
@@ -122,16 +141,16 @@ def train_net(graph, net, epochs: int, lr: float, make_animation: bool=False):
             axes[1].cla()
             axes[1].axis('off')
             axes[1].set_title(f'Epoch {i}')
-            axes[1].scatter(embeds[:, 0], embeds[:, 1], c=ncolours)
+            axes[1].scatter(logits[:, 0], logits[:, 1], c=ncolours)
 
         fig = plt.figure(dpi=100)
         axes = fig.subplots(1, 2)
-        ani = animation.FuncAnimation(fig, draw, frames=range(0, epochs, 50), 
-                                      interval=100)
+        ani = animation.FuncAnimation(fig, draw, frames=range(0, epochs, 1), 
+                                      interval=10)
         ani.save('animation.mp4')
 
 
 if __name__ == '__main__':
-    graph = build_karate_club_graph()
-    net = GCN(emb_dim=2, hidden_dim=2, num_classes=2)
-    train_net(graph, net, epochs=5000, lr=3e-3, make_animation=True)
+    graph = build_graph()
+    net = GCN(emb_dim=16, hidden_dim=32, num_classes=2)
+    train_net(graph, net, epochs=5000, lr=5e-5, make_animation=True)
